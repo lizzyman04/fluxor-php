9ca4a43e5c0fc6f2cdaf500badb13972f5702a1d
lizzyman04 <arlindoabdullm@gmail.com>
2026-05-07 03:59:23 +0200

    feat: add clear-router-cache script for Fluxor router cache

--- installer/Setup.php
+++ installer/Setup.php
@@ -46,6 +46,7 @@ class Setup
 
         unset($composer['scripts']['post-create-project-cmd']);
         $composer['scripts']['dev'] = "php -S localhost:{$appConfig['app_port']} -t public";
+        $composer['scripts']['clear-router-cache'] = "@php vendor/bin/fluxor cache:clear-router";
 
         if (isset($composer['autoload']['classmap'])) {
             unset($composer['autoload']['classmap']);
